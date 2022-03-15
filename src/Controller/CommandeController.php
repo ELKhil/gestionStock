ff4bc48fcdbac268f5756416998730a6ac325c55
ELKHIL <you@example.com>
2022-03-15 15:51:06 +0100

    version with User

--- src/Controller/CommandeController.php
+++ src/Controller/CommandeController.php
@@ -23,7 +23,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class CommandeController extends AbstractController
 {
     #[Route('/commande', name: 'commande')]
-    #[IsGranted('administrateur')]
+    #[IsGranted('admin')]
     public function commande(Request $request, CommandeRepository $repo): \Symfony\Component\HttpFoundation\Response
     {
 
